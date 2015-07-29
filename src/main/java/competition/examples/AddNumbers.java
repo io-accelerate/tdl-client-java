7de413c7060bf16595a32f5a6704066a63149616
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-07-29 21:37:35 +0100

    Improved consistency

--- src/main/java/competition/examples/AddNumbers.java
+++ src/main/java/competition/examples/AddNumbers.java
@@ -1,17 +1,14 @@
-package competition.example;
+package competition.examples;
 
 import competition.client.Client;
 
 /**
  * Created by julianghionoiu on 11/06/2015.
  */
-public class AddNumbersExample {
+public class AddNumbers {
 
     public static void main(String[] args) throws Exception {
-        String hostname = "localhost";
-        int port = 21616;
-        String username =  "your_username";
-        Client client = new Client(hostname, port, username);
+        Client client = new Client("localhost", 21616, "test");
 
         client.goLiveWith(params -> {
             Integer x = Integer.parseInt(params[0]);
