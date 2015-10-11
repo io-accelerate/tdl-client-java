c17f827aedb944eec4ea5e55172db6f190acebb7
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-10-11 20:00:19 +0100

    Replace lambda in example with anonymous class to fix cobertura

--- src/main/java/tdl/examples/AddNumbers.java
+++ src/main/java/tdl/examples/AddNumbers.java
@@ -1,6 +1,7 @@
 package tdl.examples;
 
 import tdl.client.Client;
+import tdl.client.abstractions.UserImplementation;
 
 /**
  * Created by julianghionoiu on 11/06/2015.
@@ -10,10 +11,13 @@ public class AddNumbers {
     public static void main(String[] args) throws Exception {
         Client client = new Client("localhost", 21616, "test");
 
-        client.goLiveWith(params -> {
-            Integer x = Integer.parseInt(params[0]);
-            Integer y = Integer.parseInt(params[1]);
-            return x + y;
+        client.goLiveWith(new UserImplementation() {
+            @Override
+            public Object process(String... params) {
+                Integer x = Integer.parseInt(params[0]);
+                Integer y = Integer.parseInt(params[1]);
+                return x + y;
+            }
         });
     }
 }
