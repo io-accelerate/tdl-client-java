17c53657e1caee81b0b08e2a42b40a9811694983
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-10-11 20:06:31 +0100

    Switch the example back to lambda as cobertura will now ignore the class.

--- src/main/java/tdl/examples/AddNumbers.java
+++ src/main/java/tdl/examples/AddNumbers.java
@@ -1,7 +1,6 @@
 package tdl.examples;
 
 import tdl.client.Client;
-import tdl.client.abstractions.UserImplementation;
 
 /**
  * Created by julianghionoiu on 11/06/2015.
@@ -11,13 +10,10 @@ public class AddNumbers {
     public static void main(String[] args) throws Exception {
         Client client = new Client("localhost", 21616, "test");
 
-        client.goLiveWith(new UserImplementation() {
-            @Override
-            public Object process(String... params) {
-                Integer x = Integer.parseInt(params[0]);
-                Integer y = Integer.parseInt(params[1]);
-                return x + y;
-            }
+        client.goLiveWith(params -> {
+            Integer x = Integer.parseInt(params[0]);
+            Integer y = Integer.parseInt(params[1]);
+            return x + y;
         });
     }
 }
