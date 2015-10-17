b1ce9009eb96ed0ea8115f6a1cc43190bccba798
Julian Ghionoiu <julian.ghionoiu@valtech.co.uk>
2015-10-17 13:13:11 +0100

    Improve release.sh script

--- release.sh
+++ release.sh
@@ -3,20 +3,20 @@
 SCRIPT_FOLDER="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 VERSION_FILE="${SCRIPT_FOLDER}/gradle.properties"
 SPEC_FOLDER="${SCRIPT_FOLDER}/src/test/resources/tdl/client/"
+TMP_VERSION_FILE="${SCRIPT_FOLDER}/build/versions.txt"
 
 
 function gradle_property() {
     local property=$1
-    ${SCRIPT_FOLDER}/gradlew -q ${property}
+    cat ${TMP_VERSION_FILE} | grep ${property} | cut -d "=" -f2 | tr -d " "
 }
 
 echo "Reading gradle properties. This might take some time."
 
 # Previous
-PREVIOUS_VERSION=`gradle_property printPreviousVersion`
-echo "PREVIOUS_VERSION = ${PREVIOUS_VERSION}"
-CURRENT_VERSION=`gradle_property printVersion`
-echo "CURRENT_VERSION = ${CURRENT_VERSION}"
+${SCRIPT_FOLDER}/gradlew -q printVersionInformation | tee ${TMP_VERSION_FILE}
+PREVIOUS_VERSION=`gradle_property PREVIOUS_VERSION`
+CURRENT_VERSION=`gradle_property CURRENT_VERSION`
 
 # Prompt for version confirmation
 read -p "Going to release version ${CURRENT_VERSION} (previous ${PREVIOUS_VERSION}). Proceed ? [y/n] "
