b855ccd153e47a1fc86596cc5e8cf771a1ebfc31
neomatrix369 <sadhak001@gmail.com>
2018-09-14 00:56:02 +0100

    Updated both start and stop scripts to be able to run both ActiveMQ and Amazon SQS brokers

--- startExternalDependencies.sh
+++ startExternalDependencies.sh
@@ -4,6 +4,9 @@ set -e
 set -u
 set -o pipefail
 
+BROKER_TYPE=${BROKER_TYPE:-"amazonsqs"}
+SQS_PORT=28161
+
 startWiremocks() {
     echo "~~~~~~~~~~ Starting Wiremocks on ports 41375 and 8222 ~~~~~~~~~"
     python wiremock/fetch-wiremock-and-run.py start 41375
@@ -12,7 +15,28 @@ startWiremocks() {
 
 startBroker() {
     echo "~~~~~~~~~~ Starting Broker ~~~~~~~~~"
-    python broker/activemq-wrapper.py start
+    if [[ "${BROKER_TYPE}" == "activemq" ]]; then
+        echo "~~~~~~~~~~ Broker type: ${BROKER_TYPE} ~~~~~~~~~"
+        python broker/activemq-wrapper.py start
+    else
+        IMAGE_NAME=goaws
+        echo "~~~~~~~~~~ Broker type: ${BROKER_TYPE} ~~~~~~~~~"
+        docker pull pafortin/${IMAGE_NAME}
+        docker run -d --name ${IMAGE_NAME} -p ${SQS_PORT}:4100 pafortin/${IMAGE_NAME}
+
+        export AWS_ACCESS_KEY_ID="local_test_access_key"
+        export AWS_SECRET_ACCESS_KEY="local_test_secret_key"
+        export SQS_REGION="amazonsqs"
+
+cat > ${HOME}/.aws/config <<EOL
+[default]
+region = amazonsqs
+output = json
+EOL
+
+        aws --endpoint-url http://localhost:${SQS_PORT} sqs create-queue --queue-name testuser.req
+        aws --endpoint-url http://localhost:${SQS_PORT} sqs create-queue --queue-name testuser.resp
+    fi
 }
 
 startWiremocks
--- stopExternalDependencies.sh
+++ stopExternalDependencies.sh
@@ -4,16 +4,49 @@ set -e
 set -u
 set -o pipefail
 
+BROKER_TYPE=${BROKER_TYPE:-"amazonsqs"}
+SQS_PORT=28161
+
+stopProcessAtPort() {
+    PORT=$1
+    PID=$(netstat -tulpn | grep :41375 | awk '{print $7}' | tr -d "/java" || true)
+    if [[ -z "${PID}" ]]; then
+        echo "~~~~~~~~~~ Wiremock on port ${PORT} stopped ~~~~~~~~~"
+    else
+        kill -9 ${PID}
+        echo "~~~~~~~~~~ Wiremock on port ${PORT} killed ~~~~~~~~~"
+    fi
+}
 
 stopWiremocks() {
-    echo "~~~~~~~~~~ Stoping Wiremocks listening on ports 41375 and 8222 ~~~~~~~~~"
-    python wiremock/fetch-wiremock-and-run.py stop
-    python wiremock/fetch-wiremock-and-run.py stop
+    echo "~~~~~~~~~~ Stopping Wiremocks listening on ports 41375 and 8222 ~~~~~~~~~"
+    python wiremock/fetch-wiremock-and-run.py stop || true
+
+    stopProcessAtPort 41375
+    stopProcessAtPort 8222
 }
 
 stopBroker() {
-    echo "~~~~~~~~~~ Stoping Broker ~~~~~~~~~"
-    python broker/activemq-wrapper.py stop
+    echo "~~~~~~~~~~ Stopping Broker ~~~~~~~~~"
+    if [[ "${BROKER_TYPE}" == "activemq" ]]; then
+        python broker/activemq-wrapper.py stop
+    else
+        CONTAINER_NAME=goaws
+        CONTAINER_ID=$(docker ps --filter="name=${CONTAINER_NAME}" -q)
+        echo ${CONTAINER_ID} | xargs -r docker stop || true
+        sleep 2
+        CONTAINER_ID=$(docker ps --filter="name=${CONTAINER_NAME}" -q || true)
+        if [[ -z "${CONTAINER_ID}" ]]; then
+            echo "~~~~~~~~~~ Broker Stopped ~~~~~~~~~"
+        else
+           echo "~~~~~~~~~~ Killing Broker (was still running as Container ID ${CONTAINER_ID}) ~~~~~~~~~"
+           echo ${CONTAINER_ID} | xargs -r docker kill || true
+        fi
+
+        echo "~~~~~~~~~~ Removing container ${CONTAINER_ID} ~~~~~~~~~"
+        CONTAINER_ID=$(docker ps --filter="name=${CONTAINER_NAME}" -q -a)
+        echo ${CONTAINER_ID} | xargs -r docker rm || true
+    fi    
 }
 
 stopWiremocks
