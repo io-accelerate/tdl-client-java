110fbc4c4144699de146f0a03ea8d0d1e3f61880
neomatrix369 <sadhak001@gmail.com>
2018-10-14 10:02:18 +0100

    Added the "(NOT PUBLISHED)" label to use cases when errors occur (when response type is FatalErrorResponse)

--- src/main/java/tdl/client/queue/abstractions/response/FatalErrorResponse.java
+++ src/main/java/tdl/client/queue/abstractions/response/FatalErrorResponse.java
@@ -24,7 +24,7 @@ public class FatalErrorResponse implements Response {
 
     @Override
     public String getAuditText() {
-        return String.format("%s = \"%s\"",  getId(), getResult() );
+        return String.format("%s = \"%s\", %s",  getId(), getResult(), "(NOT PUBLISHED)" );
     }
 
 }
