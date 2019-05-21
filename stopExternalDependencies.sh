1e2a26678adb9dbe9c0e5d6a2476047cf5a0d0e6
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2019-05-21 18:24:48 +0100

    Update stop script

--- stopExternalDependencies.sh
+++ stopExternalDependencies.sh
@@ -6,8 +6,8 @@ set -o pipefail
 
 startWiremocks() {
     echo "~~~~~~~~~~ Starting Wiremocks on ports 41375 and 8222 ~~~~~~~~~"
-    python3 wiremock/fetch-wiremock-and-run.py stop 41375
-    python3 wiremock/fetch-wiremock-and-run.py stop 8222
+    python3 wiremock/wiremock-wrapper.py stop 41375
+    python3 wiremock/wiremock-wrapper.py stop 8222
 }
 
 startBroker() {
